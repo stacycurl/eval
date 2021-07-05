72b9ca5a3655a9c291fa68ef991d97be7ffcb097
Stacy Curl <stacy.curl@gmail.com>
2021-07-05 18:05:54 +0100

    Generalized some more

--- src/main/scala/eval/Expr.scala
+++ src/main/scala/eval/Expr.scala
@@ -10,7 +10,7 @@ import scala.collection.immutable.{Map => SMap}
 sealed trait Expr[A] {
   def interpret(interpreter: Interpreter): Expr[A]
 
-  final def ->(rhs: A): (Expr[A], A) = (this, rhs)
+  final def :=(rhs: A): (Expr[A], A) = (this, rhs)
 }
 
 object Expr {
@@ -19,13 +19,27 @@ object Expr {
   }
   
   implicit class ExprFSyntax[A, F[_]](private val self: Expr[F[A]]) {
-    def map[B](fn: Expr[A => B])(implicit N: FName[F]): Expr[F[B]] = self |> fn.lift[F]
+    def map[B](fn: Expr[A => B])(implicit N: F1Name[F]): Expr[F[B]] = self |> fn.lift[F]
+    
+    def flatMap[B](fn: Expr[A => F[B]])(implicit N: F1Name[F]): Expr[F[B]] = self |> fn.lift 
   }
   
   implicit class ExprFnSyntax[A, B](private val self: Expr[A => B]) extends AnyVal {
     def andThen[C](f: Expr[B => C]): Expr[A => C] = AndThen(self, f)
     
-    def lift[F[_]: FName]: Expr[F[A] => F[B]] = Lift[Function, F, A, B](self, liftFExpr[Function, F])
+    def lift[F[_]: F1Name]: Expr[F[A] => F[B]] = Lift[Function, Lambda[(A, B) => F[A] => F[B]], A, B](
+      self, 
+      liftFExpr[Function, Lambda[(A, B) => F[A] => F[B]]]
+    )
+  }
+
+  implicit class ExprKleisliSyntax[F[_], A, B](private val self: Expr[A => F[B]]) extends AnyVal {
+    def andThen[C](f: Expr[B => F[C]]): Expr[A => F[C]] = >=>(self, f)
+    
+    def lift(implicit N: F1Name[F]): Expr[F[A] => F[B]] = Lift[Lambda[(A, B) => A => F[B]], Lambda[(A, B) => F[A] => F[B]], A, B](
+      self, 
+      liftFExpr[Lambda[(A, B) => A => F[B]], Lambda[(A, B) => F[A] => F[B]]]
+    )
   }
 
   case class Literal[A](value: A) extends Expr[A] {
@@ -46,20 +60,26 @@ object Expr {
     }
   }
   
+  case class >=>[F[_], A, B, C](afb: Expr[A => F[B]], bfc: Expr[B => F[C]]) extends Expr[A => F[C]] {
+    def interpret(interpreter: Interpreter): Expr[A => F[C]] = {
+      ???
+    } 
+  }
+  
   trait ~~>[F[_, _], G[_, _]] {
     def apply[A, B](fa: F[A, B]): G[A, B]
   }
   
   type Definition[A] = (Expr[A], A)
   
-  def liftFExpr[Fn[_, _], F[_]: FName]: Expr[Fn ~~> Lambda[(A, B) => F[A] => F[B]]] = 
-    Named[Fn ~~> Lambda[(A, B) => F[A] => F[B]]](s"lift-${FName.name[F]}")
+  def liftFExpr[Fn[_, _]: F2Name, Gn[_, _]: F2Name]: Expr[Fn ~~> Gn] = 
+    Name[Fn ~~> Gn](s"lift-${F2Name.name[Fn]}-to-${F2Name.name[Gn]}")
   
-  case class Lift[Fn[_, _], F[_], A, B](
+  case class Lift[Fn[_, _], Gn[_, _], A, B](
     fnExpr: Expr[Fn[A, B]], 
-    liftExpr: Expr[Fn ~~> Lambda[(A, B) => F[A] => F[B]]]
-  ) extends Expr[F[A] => F[B]] {
-    def interpret(interpreter: Interpreter): Expr[F[A] => F[B]] = {
+    liftExpr: Expr[Fn ~~> Gn]
+  ) extends Expr[Gn[A, B]] {
+    def interpret(interpreter: Interpreter): Expr[Gn[A, B]] = {
       (fnExpr.interpret(interpreter), liftExpr.interpret(interpreter)) match {
         case (Literal(ab), Literal(lift)) => Literal(lift(ab))
         case _ => this
@@ -67,8 +87,7 @@ object Expr {
     }
   }
   
-  
-  case class Named[A](name: String) extends Expr[A] {
+  case class Name[A](name: String) extends Expr[A] {
     def interpret(interpreter: Interpreter): Expr[A] = interpreter.lookup[A](this)
   }
 }
@@ -100,20 +119,42 @@ object Interpreter {
   }
 }
 
-trait FName[F[_]] {
+trait F1Name[F[_]] {
   def name: String
 }
 
-object FName {
-  def name[F[_]: FName]: String = of[F].name
+object F1Name {
+  def name[F[_]: F1Name]: String = of[F].name
   
-  def of[F[_]](implicit F: FName[F]): FName[F] = F
+  def of[F[_]](implicit F: F1Name[F]): F1Name[F] = F
   
-  implicit val listFName: FName[List] = create[List]("list")
+  implicit val listFName: F1Name[List] = create[List]("list")
   
-  implicit val optionFName: FName[Option] = create[Option]("option")
+  implicit val optionFName: F1Name[Option] = create[Option]("option")
   
-  private def create[F[_]](name0: String): FName[F] = new FName[F] {
+  private def create[F[_]](name0: String): F1Name[F] = new F1Name[F] {
+    val name: String = name0
+  }
+}
+
+trait F2Name[F[_, _]] {
+  def name: String
+}
+
+object F2Name {
+  def name[F[_, _]: F2Name]: String = of[F].name
+  
+  def of[F[_, _]](implicit F: F2Name[F]): F2Name[F] = F
+  
+  implicit val functionName: F2Name[Function] = create[Function]("function")
+  
+  implicit def kleisli[F[_]: F1Name]: F2Name[Lambda[(A, B) => A => F[B]]] = 
+    create[Lambda[(A, B) => A => F[B]]](s"kleisli-in-${F1Name.name[F]}")
+  
+  implicit def liftedFunction[F[_]: F1Name]: F2Name[Lambda[(A, B) => F[A] => F[B]]] = 
+    create[Lambda[(A, B) => F[A] => F[B]]](s"list-function-to-${F1Name.name[F]}")
+  
+  private def create[F[_, _]](name0: String): F2Name[F] = new F2Name[F] {
     val name: String = name0
   }
 }
--- src/test/scala/eval/ExprSpec.scala
+++ src/test/scala/eval/ExprSpec.scala
@@ -3,7 +3,7 @@ package eval
 import scala.language.higherKinds
 
 import org.scalatest.{FreeSpec, Matchers}
-import scalaz.Functor
+import scalaz.{Functor, Monad}
 import scalaz.std.list._
 import scalaz.std.option._
 
@@ -12,26 +12,51 @@ class ExprSpec extends FreeSpec with Matchers {
   "interpret" in {
     import Expr._
     import Interpreter._
-    
-    val greeting  = Named[String]("greeting")
-    
-    val reverse   = Named[String => String]("reverse")
-    val upperCase = Named[String => String]("upper-case")
-    val lowerCase = Named[String => String]("lower-case")
-    
-    def functorDefinition[F[_]: Functor: FName]: Definition[Function ~~> Lambda[(A, B) => F[A] => F[B]]] = {
-      Expr.liftFExpr[Function, F] -> new (Function ~~> Lambda[(A, B) => F[A] => F[B]]) {
+
+    val greeting  = Name[String]("greeting")
+
+    val reverse   = Name[String => String]("reverse")
+    val upperCase = Name[String => String]("upper-case")
+    val lowerCase = Name[String => String]("lower-case")
+
+    val tails = Name[String => List[String]]("tails")
+    val vowels = Name[String => Option[String]]("vowels")
+
+    val vowelsImpl: String => Option[String] = value => {
+      val filtered = value.filter(Set('a', 'e', 'i', 'o', 'u'))
+
+      if (filtered.isEmpty) None else Some(filtered)
+    }
+
+    def functorDefinition[F[_]: Functor: F1Name]: Definition[Function ~~> Lambda[(A, B) => F[A] => F[B]]] = {
+      Expr.liftFExpr[Function, Lambda[(A, B) => F[A] => F[B]]] -> new (Function ~~> Lambda[(A, B) => F[A] => F[B]]) {
         def apply[A, B](fa: A => B): F[A] => F[B] = Functor[F].lift(fa)
       }
     }
-    
+
+    def monadDefinition[M[_]: Monad: F1Name]: Definition[Lambda[(A, B) => A => M[B]] ~~> Lambda[(A, B) => M[A] => M[B]]] = (
+      Expr.liftFExpr[Lambda[(A, B) => A => M[B]], Lambda[(A, B) => M[A] => M[B]]],
+      new (Lambda[(A, B) => A => M[B]] ~~> Lambda[(A, B) => M[A] => M[B]]) {
+        def apply[A, B](fa: A => M[B]): M[A] => M[B] = Monad[M].bind(_)(fa)
+      }
+    )
+
     val interpreter = Interpreter.Real(
       Definitions(
-        greeting   -> "Ola !", 
-        reverse    -> (_.reverse),
-        upperCase  -> (_.toUpperCase),
+        greeting   := "Ola !",
+        reverse    := (_.reverse),
+        upperCase  := (_.toUpperCase),
+        vowels     := vowelsImpl,
+        tails      := (_.tails.toList),
         functorDefinition[Option],
         functorDefinition[List],
+        monadDefinition[Option],
+        monadDefinition[List],
+//        Expr.liftFExpr[Lambda[(A, B) => A => List[B]], Lambda[(A, B) => List[A] => List[B]]] -> new (
+//          Lambda[(A, B) => A => List[B]] ~~> Lambda[(A, B) => List[A] => List[B]]
+//        ) {
+//          def apply[A, B](fa: A => List[B]): List[A] => List[B] = _.flatMap(fa)
+//        }
 //        Expr.liftFExpr[List] -> new Expr.LiftFunction[List] {
 //          def apply[A, B](fa: A => B): List[A] => List[B] = _.map(fa)
 //        },
@@ -40,15 +65,18 @@ class ExprSpec extends FreeSpec with Matchers {
 //        }
       )
     )
-    
+
     assert(Literal("olleh") === interpreter.interpret(Literal("hello") |> reverse))
     assert(Literal("HELLO") === interpreter.interpret(Literal("hello") |> upperCase))
     assert(Literal("OLLEH") === interpreter.interpret(Literal("hello") |> upperCase.andThen(reverse)))
     assert((Literal("hello") |> upperCase.andThen(lowerCase)) === interpreter.interpret(Literal("hello") |> upperCase.andThen(lowerCase)))
-    
+
     assert(Literal("! alO") === interpreter.interpret(greeting |> reverse))
 
     assert(Literal(List("olleh")) === interpreter.interpret(Literal(List("hello")).map(reverse)))
     assert(Literal(Some("HELLO")) === interpreter.interpret(Literal(Option("hello")).map(upperCase)))
+
+    assert(Literal(List("hello", "ello", "llo", "lo", "o", "")) === interpreter.interpret(Literal(List("hello")).flatMap(tails)))
+    assert(Literal(Some("eo")) === interpreter.interpret(Literal(Option("hello")).flatMap(vowels)))
   }
 }
